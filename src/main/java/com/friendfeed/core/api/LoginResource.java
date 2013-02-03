26193fd06f553e13d02746930df8aa285de8296d
Joao Carvalho <joao.pedro.carvalho@ist.utl.pt>
2013-02-03 02:10:12 +0000

    Added basic RequestToken for Twitter

--- src/main/java/com/friendfeed/core/api/LoginResource.java
+++ src/main/java/com/friendfeed/core/api/LoginResource.java
@@ -5,15 +5,24 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import com.friendfeed.core.domain.FriendFeed;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.RequestToken;
 
 @Path("/login")
 public class LoginResource extends AbstractResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String getLogin() {
-	return "hello " + FriendFeed.getInstance();
+    public String getLogin() throws TwitterException {
+
+	Twitter twitter = TwitterFactory.getSingleton();
+
+	RequestToken token = twitter.getOAuthRequestToken("http://localhost:8080/friendfeed/api/logged");
+
+	return token.getAuthenticationURL();
+
     }
 
 }
