c09fc4d761896ca588e2fc2161f5786758dd97f7
Joao Carvalho <joao.pedro.carvalho@ist.utl.pt>
2013-02-03 01:42:17 +0000

    Added ConfigurationManager

---
+++ src/main/java/com/friendfeed/core/util/ConfigurationManager.java
@@ -0,0 +1,28 @@
+package com.friendfeed.core.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ConfigurationManager {
+
+    private static final Properties properties = new Properties();
+
+    static {
+	try {
+	    InputStream stream = ConfigurationManager.class.getClassLoader().getResourceAsStream("/configuration.properties");
+	    properties.load(stream);
+	} catch (IOException e) {
+	    throw new Error(e);
+	}
+    }
+
+    public static String getConsumerKey() {
+	return properties.getProperty("oauth.consumerkey");
+    }
+
+    public static String getConsumerSecret() {
+	return properties.getProperty("oauth.consumersecret");
+    }
+
+}
